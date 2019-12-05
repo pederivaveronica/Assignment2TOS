ddea9d7d80c1c871d8ed0ef14b01ed973fbc44b6
pederivaveronica <veronicapederiva@MacBook-Pro-di-Veronica.local>
2019-12-05 15:06:55 +0100

    close #2

--- src/main/java/it/unipd/tos/myTakeAwayBill.java
+++ src/main/java/it/unipd/tos/myTakeAwayBill.java
@@ -14,15 +14,33 @@ public class myTakeAwayBill implements TakeAwayBill {
 	public double getOrderPrice(List<MenuItem> itemsOrdered) 
 			throws TakeAwayBillException {
 		
-		//totale ordine
+		//totale
 		double totale = 0;
-		
-		//caso 1
+		//numero totale panini
+        int numPanini = 0;
+        //costo minimo panino
+        double minPrice = Double.MAX_VALUE;
+
+
+        //caso 1
         for (int i = 0; i < itemsOrdered.size(); i++) {
             totale += itemsOrdered.get(i).getPrice();
+
+            if (itemsOrdered.get(i).getType() == itemType.Panini) {
+                numPanini++;
+                if (itemsOrdered.get(i).getPrice() < minPrice) {
+                    minPrice = itemsOrdered.get(i).getPrice();
+                }
+            }
+
+          
         }
 
-		
+        //caso 2
+        if (numPanini > 5) {
+            totale -= minPrice;
+            totale += minPrice * 0.5;
+        }
 		return totale;
 	}
 	
